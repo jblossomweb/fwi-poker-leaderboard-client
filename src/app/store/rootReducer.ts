1d129f3f9471879d0e065916a911a0f41a6f0b36
John Blossom <jblossom.web@gmail.com>
2019-03-07 02:24:57 -0700

    players added to root reducer

--- src/app/store/rootReducer.ts
+++ src/app/store/rootReducer.ts
@@ -3,11 +3,11 @@ import { combineAllReducers } from 'core/store/reducer.utils'
 import { AppReducers } from 'core/store/reducer.types'
 
 // import your reducers here.
-import sampleReducers from './sample/sample.reducers'
+import playerReducers from './players/action.reducers'
 
 export const appReducers: AppReducers = extend({},
   // register your reducers here.
-  sampleReducers,
+  playerReducers,
 )
 
 export default combineAllReducers(appReducers)
